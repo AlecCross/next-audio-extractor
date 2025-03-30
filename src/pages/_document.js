4e8e25db0e0e73b539a89db59cb75f265cf45113
AlecCross <cross2472@gmail.com>
2025-03-30 15:41:15 +0000

    fix icon for tab

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/icon-16.ico" />
+        <link rel="icon" type="image/webp" sizes="32x32" href="/icon-32.webp" />
+        <link rel="icon" type="image/webp" sizes="16x16" href="/icon-16.webp" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       <body>
         <Main />
         <NextScript />
