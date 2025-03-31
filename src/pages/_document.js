2484d700fd4d6b54a682b4e23de19fb95b434074
AlecCross <cross2472@gmail.com>
2025-03-31 18:41:49 +0000

    fix ofline work

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,6 +7,9 @@ export default function Document() {
         <link rel="icon" href="/icon-16.ico" />
         <link rel="icon" type="image/webp" sizes="32x32" href="/icon-32.webp" />
         <link rel="icon" type="image/webp" sizes="16x16" href="/icon-16.webp" />
+        <link rel="apple-touch-icon" href="/icon-32.webp"/>
+        <meta property="og:type" content="app"/>
+        <meta property="og:image" content="/icon-512.webp"/>
         <link rel="manifest" href="/manifest.json" />
       </Head>
       <body>
